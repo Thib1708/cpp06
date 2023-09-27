770cb2a93572f30282d5e97ac65007f97a69c091
Thibault Giraudon <tgiraudo@z4r9p5.42lyon.fr>
2023-09-27 16:05:00 +0200

    ex01 does not compile on linux

--- ex01/include/Serializer.hpp
+++ ex01/include/Serializer.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Serializer.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thibaultgiraudon <thibaultgiraudon@stud    +#+  +:+       +#+        */
+/*   By: tgiraudo <tgiraudo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 13:11:57 by thibaultgir       #+#    #+#             */
-/*   Updated: 2023/09/13 15:58:47 by thibaultgir      ###   ########.fr       */
+/*   Updated: 2023/09/27 16:04:38 by tgiraudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define SERIALIZER_HPP
 
 #include <iostream>
+#include <stdint.h>
 
 struct Data
 {
